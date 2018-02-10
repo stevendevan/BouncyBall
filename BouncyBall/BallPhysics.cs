4d6f5e3c4555a3b02d1de1e66a83ebdfb5d1c621
stevendevan <stevendevan12@gmail.com>
2018-02-10 15:49:17 -0800

    Created BallPhysics object for simulating the ball physics and integrated into project. Some fine tuning and collision logic still needs to be implemented.

---
+++ BouncyBall/BallPhysics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BouncyBall
+{
+    class BallPhysics
+    {
+        private float dt;
+        private float g = 2000.0f;
+        private float damping = 5.0f;
+        private int xPos;
+        private int yPos;
+        private float xVel;
+        private float yVel;
+
+        public BallPhysics(int xCurr, int xPrev, int yCurr, int yPrev, float dt)
+        {
+            this.dt = dt;
+            this.xPos = xCurr;
+            this.yPos = yCurr;
+            this.xVel = (int)((xCurr - xPrev) / dt);
+            this.yVel = (int)((yCurr - yPrev) / dt);
+        }
+
+        public int DeltaPositionX()
+        {
+            this.xVel += this.damping * this.dt;
+            this.xPos += (int)(this.xVel * this.dt);
+
+            return this.xPos;
+        }
+
+        public int DeltaPositionY()
+        {
+            this.yVel += this.g * this.dt;
+            this.yPos += (int)(this.yVel * this.dt);
+
+            return this.yPos;
+        }
+
+        public int xPosition { get; set; }
+        public int yPosition { get; set; }
+    }
+}
--- BouncyBall/FormMain.cs
+++ BouncyBall/FormMain.cs
@@ -12,20 +12,26 @@ namespace BouncyBall
 {
     public partial class FormMain : Form
     {
-        //private Ball ball = new Ball();
-        bool mouseDownLeft = false;
+        private BallPhysics Physics;
+        private bool ResetPhysics;
+        private bool mouseDownLeft = false;
         private int radius = 40;
+        private int xPrev = 0;
+        private int yPrev = 0;
+        //private int xCurr = 0;
+        //private int yCurr = 0;
 
         public FormMain()
         {
             InitializeComponent();
+            this.timerPhysics.Enabled = true;
         }
 
         private void FormMain_MouseMove(object sender, MouseEventArgs e)
         {
             if (mouseDownLeft)
             {
-                ball.Location = new Point(e.Location.X - (int)(this.radius / 2), e.Location.Y - (int)(this.radius / 2));
+                this.ball.Location = new Point(e.X - (int)(this.radius / 2), e.Y - (int)(this.radius / 2));
                 //ball.Update(); //not sure if this is needed. Invalidate() causes flickering
             }
         }
@@ -48,14 +54,13 @@ namespace BouncyBall
         {
             if (e.Button == MouseButtons.Left)
             {
+                //this.timerPhysics.Enabled = true;
                 this.mouseDownLeft = true;
                 this.ball.Radius = radius;
                 this.ball.Width = radius;
                 this.ball.Height = radius;
                 this.ball.Visible = true;
-                this.ball.Location = new Point((int)(this.Width - this.label1.Width) / 2, (int)(this.Height - this.label1.Height) / 2);
-                //ball.Location = new Point(e.Location.X, e.Location.Y);
-                //ball.Invalidate();
+                this.ball.Location = new Point((int)(e.X - this.ball.Radius) / 2, (int)(e.Y - this.ball.Radius) / 2);
                 this.label1.Visible = false;
             }
         }
@@ -66,14 +71,31 @@ namespace BouncyBall
             {
                 this.label1.Visible = true;
                 this.ball.Visible = false;
+                //this.timerPhysics.Enabled = false;
             }
         }
 
         private void TimerPhysics_Tick(object sender, EventArgs e)
         {
+            if (mouseDownLeft == true)
+            {
+                ResetPhysics = true;
+                xPrev = MousePosition.X;
+                yPrev = MousePosition.Y;
+            }
+
             if (mouseDownLeft == false)
             {
-                this.ball.Location = new Point(50,50);
+                if (ResetPhysics == true)
+                {
+                    Physics = new BallPhysics(MousePosition.X, xPrev, MousePosition.Y, yPrev, timerPhysics.Interval * .001f);
+                    ResetPhysics = false;
+                }
+                
+                if (Physics != null)
+                {
+                    this.ball.Location = new Point(Physics.DeltaPositionX() - (this.ball.Radius / 2), Physics.DeltaPositionY() - (this.ball.Radius / 2));
+                }
             }
         }
 
